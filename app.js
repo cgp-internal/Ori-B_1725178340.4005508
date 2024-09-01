4281403b15b8c421364634e3fea0b8d5ca5aa5fa
cgp-client <code.gen.pro.system@gmail.com>
2024-09-01 11:24:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { notesRouter } = require('./routes/notes');
+const notesRouter = require('./routes/notes');
 
 const app = express();
 app.use(express.json());
 
 app.use('/notes', notesRouter);
+
+if (typeof notesRouter === 'undefined') {
+  throw new Error('Notes router is not defined');
+}
 
 const PORT = 3000;
 app.listen(PORT, () => {
